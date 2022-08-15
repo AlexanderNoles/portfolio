3910c08fbd5ace078923ee846dd562d0c6c73bdf
AlexanderNoles <alexnoles@outlook.com>
2022-08-15 15:09:07 +0100

    intial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import MainTitle from './components/MainTitle.vue'
+</script>
+
+<template>
+<MainTitle mainTitle = "Spook" subTitle = "Video game development and pixel art" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  mainTitle: {
+    type: String,
+    required: true
+  },
+  subTitle: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class = "base"> 
+    <div class = "mainTitle">
+      {{ mainTitle }}
+      <div class = "subTitle">
+        {{ subTitle }}
+      </div>
+    </div> 
+  </div>
+
+</template>
+<style scoped>
+.base{
+  position: fixed;
+  padding: 5px;
+  padding-top: 0px;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: #242424;
+
+  -webkit-user-select: none; /* Safari */        
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+}
+.mainTitle {
+  font-size: 35px;
+  text-align: center;
+}
+.subTitle {
+  font-size: 20px;
+}
+</style>
